22171d8799922dd61bee6f405490e6d7c8d7f85e
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-08-05 20:53:44 +0530

    Zod added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,18 @@ app.get("/", (req,res)=>{
     return res.send("Hi There")
 })
 
+app.post('/todo', (req,res)=>{
+    return res.send("Hi There")
+})
+
+app.get('/todos', (req,res)=>{
+    return res.send("Hi There")
+})
+
+app.put('/completed', (req,res)=>{
+    return res.send("Hi There")
+})
+
 app.listen(PORT, () => {
     console.log("Server running on PORT "+PORT)
 })
